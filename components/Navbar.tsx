b724a19ee534a7b7a1319e48a37c2dda66f2d0c0
Zhuo <nicodiazan@gmail.com>
2025-04-20 09:01:48 +0200

    Add a basic layout and the basis for a Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Menu } from "lucide-react";
+import Link from "next/link";
+
+const Navbar = () => {
+	return (
+		<div className="fixed w-full z-50 justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+			<div className="flex items-center">
+				<Menu className="block md:hidden" />
+                <Link href="/" >
+                 <h1 className="hidden md:block text-xl md:text-3xl font-bold text-primary">
+                    aporia
+                 </h1>
+                </Link>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
