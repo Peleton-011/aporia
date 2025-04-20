9faaea5811ad3716f464d936b8bf54f6439cd42c
Zhuo <nicodiazan@gmail.com>
2025-04-20 09:57:07 +0200

    Add basis for the sidebar routes and route handling

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,15 @@
 "use client";
 
+import { Home } from "lucide-react";
+
 export const Sidebar = () => {
+	const routes = [
+		{
+			icon: Home,
+			href: "/",
+			label: "Home",
+		},
+	];
 	return (
 		<div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
 			<div className="p-3 flex-1 justify-center">
